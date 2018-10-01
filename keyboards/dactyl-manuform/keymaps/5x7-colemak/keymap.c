d81685836f2de1fb4a48f9e3fb33a70cfd752317
lachlanh <lhespe@opentable.com>
2018-09-30 22:31:24 -0700

    more layout

--- keyboards/dactyl-manuform/keymaps/5x7-colemak/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x7-colemak/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* KC_LSHIFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_LCTRL, */
    /* KC_LCTRL,  KC_LALT, KC_F6,   KC_F7,  KC_F8,  KC_BSPC, KC_LALT,  */
 
-   KC_5,    KC_4,    KC_3,    KC_2,   KC_1,   KC_ESC, 
-   MO(_FN),   KC_Q,    KC_W,    KC_F,   KC_P,   KC_G,
-   KC_TAB,    KC_A,    KC_R,    KC_S,   KC_T,   KC_D,    KC_LSHIFT,
-   KC_LSHIFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_LCTRL,
-   KC_LCTRL,  KC_LALT, KC_F6,   KC_F7,  KC_F8,  KC_BSPC, KC_LALT, 
+              KC_5,    KC_4,    KC_3,    KC_2,   KC_1,   KC_ESC, 
+              KC_G,    KC_P,    KC_F,    KC_W,   KC_Q,   KC_GRV,
+   KC_LSHIFT, KC_D,    KC_T,    KC_S,    KC_R,   KC_A,   KC_TAB,
+   KC_LGUI,   KC_B,    KC_V,    KC_C,    KC_X,   KC_Z,   KC_LSHIFT,
+   MO(_FN),   KC_BSPC, KC_RBRC, KC_LBRC, KC_F6,  KC_LALT,KC_LCTRL, 
 
    
    /* KC_DOWN,   KC_UP, KC_BSPC, KC_LSFT,MO(_FN),    KC_LCTRL, */
@@ -39,10 +39,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_EQL,
               KC_ENTER, KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT, 
               KC_RGUI,  KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
-              MO(_FN),  KC_SPC,  KC_LEFT,KC_UP,   KC_DOWN, KC_RIGHT,RESET),
+              MO(_FN),  KC_SPC,  KC_LEFT,KC_DOWN,   KC_UP, KC_RIGHT,RESET),
 
                      /* KC_RCTRL,MO(_FN),KC_ENTER,KC_SPC,  KC_LEFT, KC_RIGHT, */
                      /* KC_RALT, KC_RGUI), */
+[_FN] = KEYMAP_5x7(
+  // left hand
+   /* _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  */
+   /* _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______, */
+   /* _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_LBRC, */
+   /* RESET,    _______,  _______,  _______,  _______, _______,
+   KC_DOWN,   KC_UP, KC_BSPC, KC_LSFT,MO(_FN),    KC_LCTRL, */
+   /*                                      KC_LGUI, KC_LALT, */
+              KC_F5,   KC_F4,   KC_F3,   KC_F2,  KC_F1,  _______, 
+              _______, _______, _______, _______,_______,_______,
+   _______,   KC_LBRC, _______, _______, _______,_______,_______,
+   _______,   _______, _______, _______, _______,_______,_______,
+   MO(_FN),   _______, _______, _______, _______,_______,_______, 
+
+
+   // right hand
+                     /* KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11, */
+                     /* _______,  KC_HOME,  KC_PGDN,  KC_PGUP,   KC_END,  KC_F12, */
+                     /* KC_RBRC,  KC_LEFT,  KC_DOWN,  KC_UP,     KC_RIGHT, _______, */
+                     /* _______,  _______,  _______,  _______,   _______,   RESET, */
+                     /* KC_RCTRL,MO(_FN),KC_ENTER,KC_SPC,  KC_LEFT, KC_RIGHT, */
+                     /* KC_RALT, KC_RGUI), */
+                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+                        _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_F12,
+              _______,  KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, 
+              _______,  _______, _______, _______, _______, _______, _______,
+              MO(_FN),  _______, _______, _______, _______, _______, RESET),
 
    };
 
